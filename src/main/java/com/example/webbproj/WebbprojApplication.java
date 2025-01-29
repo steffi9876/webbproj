bc294245c77efe1ae6da6250360af1fde055542e
Stephanie <stephanie.zevian@yh.nackademin.se>
2025-01-29 15:09:29 +0100

    Initial start

---
+++ src/main/java/com/example/webbproj/WebbprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.webbproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebbprojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webbproj/WebbprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webbproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
